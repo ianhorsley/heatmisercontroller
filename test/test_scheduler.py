7793138a40ac62f63e7b726431810621df675c8b
ianhorsley <ianspam@horsley.me.uk>
2018-06-03 09:52:38 +0100

    added schedule funcs module and fixed getnextday

---
+++ test/test_scheduler.py
@@ -0,0 +1,29 @@
+import unittest
+import logging
+#import time
+
+from heatmisercontroller.schedule_functions import schedulerdayheat
+#from heatmisercontroller.hm_constants import HMV3_ID, PRT_HW_MODEL, PROG_MODE_DAY
+#from heatmisercontroller.exceptions import hmResponseError
+
+class test_schedulerdayheat(unittest.TestCase):
+  def setUp(self):
+    logging.basicConfig(level=logging.ERROR)
+    #network, address, protocol, short_name, long_name, model, mode
+    #self.func = hmController(None, 1, HMV3_ID, 'test', 'test controller', PRT_HW_MODEL, PROG_MODE_DAY)
+      
+  def test_create(self):
+    #unique_address,length,divisor, valid range
+    self.func = schedulerdayheat()
+    self.func.set_raw_all([7,0,21,9,0,12,17,0,21,22,30,15])
+    self.func.set_raw('tues_heat',[7,0,21,9,0,12,17,0,21,24,30,15])
+    self.func.display()
+    
+  def test_getnextday(self):
+    self.func = schedulerdayheat()
+    self.assertEqual(3, self.func._getNextDay([2,0,0,0]) )
+    self.assertEqual(1, self.func._getNextDay([7,0,0,0]) )
+      
+if __name__ == '__main__':
+    unittest.main()
+
