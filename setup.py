8c94f467ba456b7e1afa7f8667066e74ea7a62a2
Ian Horsley <39375170+ianhorsley@users.noreply.github.com>
2018-06-19 23:45:47 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ def readme():
         return f.read()
 
 setup(name='heatmisercontroller',
-      version='0.1',
+      version='0.2',
       description='Python implementation of Heatmiser protocol for serial connected thermostats ',
       long_description=readme(),
       classifiers=[
