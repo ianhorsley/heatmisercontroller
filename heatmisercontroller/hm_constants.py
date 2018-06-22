5a21120ce732f79011e104bf1b60598f75d47973
ianhorsley <ianspam@horsley.me.uk>
2018-06-22 08:35:47 +0100

    added maxages to fields and removed unused constants

--- heatmisercontroller/hm_constants.py
+++ heatmisercontroller/hm_constants.py
@@ -44,15 +44,6 @@ BROADCAST_ADDR = 0xff
 RW_LENGTH_ALL = 0xffff
 DCB_START = 0x00
 
-SET_TEMP_ADDR = 18
-KEY_LOCK_ADDR = 22
-HOL_HOURS_LO_ADDR = 24
-HOL_HOURS_HI_ADDR = 25
-CUR_TIME_ADDR = 43
-
-KEY_LOCK_UNLOCK = 0
-KEY_LOCK_LOCK = 1
-
 # Define frame send contents
 FS_DEST_ADDR = 0
 FS_LEN = 1
@@ -71,22 +62,10 @@ FR_CONT_LEN_LOW = 7
 FR_CONT_LEN_HIGH = 8
 FR_CONTENTS = 9
 
-# Define payload response contents
-PL_LEN_HIGH = 0
-PL_LEN_LOW = 1 
-PL_VENDOR_ID = 2
-#PL_LIMITS = 3
-PL_MODEL = 4
-PL_PROG_MODE = 16
-
-# Define DCB write addresses
-UNIQUE_WRITEABLE_ADDRESSES = [17,18,19,21,22,23,24,32,42,43]
-UNIQUE_WRITEABLE_ABOVE = 47
-
-DCB_ON_OFF = 21
-DCB_RUN_MODE = 23
-
-UNIQUE_ADD_MODEL = 4
+MAX_AGE_LONG = 86400
+MAX_AGE_MEDIUM = 3600
+MAX_AGE_SHORT = 65
+MAX_AGE_USHORT = 11
 
 FIELD_NAME_LENGTH = 13
 MAX_UNIQUE_ADDRESS = 298
@@ -96,60 +75,61 @@ FIELD_ADD = 1
 FIELD_LEN = 2
 FIELD_DIV = 3
 FIELD_RANGE = 4
-FIELD_WRITE = 5
+FIELD_MAX_AGE = 5
+FIELD_WRITE = 6
 fields = [
-['DCBlen',0,2,1,[]],
-['vendor',2,1,1,[0,1]], #00 heatmiser, 01 OEM
-['version',3,1,1,[]],
-['model',4,1,1,[0,5]], # DT/DT-E/PRT/PRT-E 00/01/02/03
-['tempformat',5,1,1,[0,1]], # 00 C, 01 F
-['switchdiff',6,1,1,[1,3]],
-['frostprot',7,1,1,[0,1]], #0=enable frost prot when display off, (opposite in protocol manual, but tested and user guide is correct)  (default should be enabled)
-['caloffset',8,2,1,[]],
-['outputdelay',10,1,1,[0,15]], # minutes (to prevent rapid switching)
-['address',11,1,1,[SLAVE_ADDR_MIN,SLAVE_ADDR_MAX]],
-['updwnkeylimit',12,1,1,[0,10]],  #limits use of up and down keys
-['sensorsavaliable',13,1,1,[0,4]], #00 built in only, 01 remote air only, 02 floor only, 03 built in + floor, 04 remote + floor
-['optimstart',14,1,1,[0,3]], # 0 to 3 hours, default 0
-['rateofchange',15,1,1,[]], #number of minutes per degree to raise the temperature, default 20. Applies to the Wake and Return comfort levels (1st and 3rd)
-['programmode',16,1,1,[0,1]], #0=5/2, 1= 7day
-['frosttemp',17,1,1,[7,17],'W'], #default is 12, frost protection temperature
-['setroomtemp',18,1,1,[5,35],'W'],
-['floormaxlimit',19,1,1,[20,45],'W'],
-['floormaxlimitenable',20,1,1,[0,1]], #1=enable
-['onoff',21,1,1,[0,1],'W'], #1 = on
-['keylock',22,1,1,[0,1],'W'], #1 = on
-['runmode',23,1,1,[0,1],'W'],  #0 = heating mode, 1 = frost protection mode
-['holidayhours',24,2,1,[0,720],'W'], #range guessed and tested, setting to 0 cancels hold and puts back to program 
-['unknown',26,6,1,[]], # gap from 26 to 31
-['tempholdmins',32,2,1,[0,5760],'W'], #range guessed and tested, setting to 0 cancels hold and puts setroomtemp back to program
-['remoteairtemp',34,2,10,[]], #ffff if no sensor
-['floortemp',36,2,10,[]], #ffff if no sensor
-['airtemp',38,2,10,[]], #ffff if no sensor
-['errorcode',40,1,1,[0,3]], # 0 is no error # errors, 0 built in, 1, floor, 2 remote
-['heatingdemand',41,1,1,[0,1]], #0 none, 1 heating currently
-['hotwaterdemand',42,1,1,[0,3],'W'], # read [0=off,1=on], write [0=as prog,1=override on,2=overide off]
-['currenttime',43,4,1,[[1,7],[0,23],[0,59],[0,59]],'W'], #day (Mon - Sun), hour, min, sec.
+['DCBlen',0,2,1,[],MAX_AGE_LONG],
+['vendor',2,1,1,[0,1],MAX_AGE_LONG], #00 heatmiser, 01 OEM
+['version',3,1,1,[],MAX_AGE_LONG],
+['model',4,1,1,[0,5],MAX_AGE_LONG], # DT/DT-E/PRT/PRT-E 00/01/02/03
+['tempformat',5,1,1,[0,1],MAX_AGE_LONG], # 00 C, 01 F
+['switchdiff',6,1,1,[1,3],MAX_AGE_LONG],
+['frostprot',7,1,1,[0,1],MAX_AGE_LONG], #0=enable frost prot when display off, (opposite in protocol manual, but tested and user guide is correct)  (default should be enabled)
+['caloffset',8,2,1,[],MAX_AGE_LONG],
+['outputdelay',10,1,1,[0,15],MAX_AGE_LONG], # minutes (to prevent rapid switching)
+['address',11,1,1,[SLAVE_ADDR_MIN,SLAVE_ADDR_MAX],MAX_AGE_LONG],
+['updwnkeylimit',12,1,1,[0,10],MAX_AGE_LONG],  #limits use of up and down keys
+['sensorsavaliable',13,1,1,[0,4],MAX_AGE_LONG], #00 built in only, 01 remote air only, 02 floor only, 03 built in + floor, 04 remote + floor
+['optimstart',14,1,1,[0,3],MAX_AGE_LONG], # 0 to 3 hours, default 0
+['rateofchange',15,1,1,[],MAX_AGE_LONG], #number of minutes per degree to raise the temperature, default 20. Applies to the Wake and Return comfort levels (1st and 3rd)
+['programmode',16,1,1,[0,1],MAX_AGE_LONG], #0=5/2, 1= 7day
+['frosttemp',17,1,1,[7,17],MAX_AGE_LONG,'W'], #default is 12, frost protection temperature
+['setroomtemp',18,1,1,[5,35],MAX_AGE_USHORT,'W'],
+['floormaxlimit',19,1,1,[20,45],MAX_AGE_LONG,'W'],
+['floormaxlimitenable',20,1,1,[0,1],MAX_AGE_LONG], #1=enable
+['onoff',21,1,1,[0,1],MAX_AGE_SHORT,'W'], #1 = on
+['keylock',22,1,1,[0,1],MAX_AGE_SHORT,'W'], #1 = on
+['runmode',23,1,1,[0,1],MAX_AGE_SHORT,'W'],  #0 = heating mode, 1 = frost protection mode
+['holidayhours',24,2,1,[0,720],MAX_AGE_SHORT,'W'], #range guessed and tested, setting to 0 cancels hold and puts back to program 
+['unknown',26,6,1,[],MAX_AGE_LONG], # gap from 26 to 31
+['tempholdmins',32,2,1,[0,5760],MAX_AGE_SHORT,'W'], #range guessed and tested, setting to 0 cancels hold and puts setroomtemp back to program
+['remoteairtemp',34,2,10,[],MAX_AGE_USHORT], #ffff if no sensor
+['floortemp',36,2,10,[],MAX_AGE_USHORT], #ffff if no sensor
+['airtemp',38,2,10,[],MAX_AGE_USHORT], #ffff if no sensor
+['errorcode',40,1,1,[0,3],MAX_AGE_SHORT], # 0 is no error # errors, 0 built in, 1, floor, 2 remote
+['heatingdemand',41,1,1,[0,1],MAX_AGE_USHORT], #0 none, 1 heating currently
+['hotwaterdemand',42,1,1,[0,3],MAX_AGE_USHORT,'W'], # read [0=off,1=on], write [0=as prog,1=override on,2=overide off]
+['currenttime',43,4,1,[[1,7],[0,23],[0,59],[0,59]],MAX_AGE_USHORT,'W'], #day (Mon - Sun), hour, min, sec.
 #5/2 progamming #if hour = 24 entry not used
-['wday_heat',47,12,1,[[0,24],[0,59],[5,35]],"W"], #hour, min, temp  (should minutes be only 0 and 30?)
-['wend_heat',59,12,1,[[0,24],[0,59],[5,35]],"W"],
-['wday_water',71,16,1,[[0,24],[0,59]],"W"], # pairs, on then off repeated, hour, min
-['wend_water',87,16,1,[[0,24],[0,59]],"W"],
+['wday_heat',47,12,1,[[0,24],[0,59],[5,35]],MAX_AGE_MEDIUM,"W"], #hour, min, temp  (should minutes be only 0 and 30?)
+['wend_heat',59,12,1,[[0,24],[0,59],[5,35]],MAX_AGE_MEDIUM,"W"],
+['wday_water',71,16,1,[[0,24],[0,59]],MAX_AGE_MEDIUM,"W"], # pairs, on then off repeated, hour, min
+['wend_water',87,16,1,[[0,24],[0,59]],MAX_AGE_MEDIUM,"W"],
 #7day progamming
-['mon_heat',103,12,1,[[0,24],[0,59],[5,35]],"W"],
-['tues_heat',115,12,1,[[0,24],[0,59],[5,35]],"W"],
-['wed_heat',127,12,1,[[0,24],[0,59],[5,35]],"W"],
-['thurs_heat',139,12,1,[[0,24],[0,59],[5,35]],"W"],
-['fri_heat',151,12,1,[[0,24],[0,59],[5,35]],"W"],
-['sat_heat',163,12,1,[[0,24],[0,59],[5,35]],"W"],
-['sun_heat',175,12,1,[[0,24],[0,59],[5,35]],"W"],
-['mon_water',187,16,1,[[0,24],[0,59]],"W"],
-['tues_water',203,16,1,[[0,24],[0,59]],"W"],
-['wed_water',219,16,1,[[0,24],[0,59]],"W"],
-['thurs_water',235,16,1,[[0,24],[0,59]],"W"],
-['fri_water',251,16,1,[[0,24],[0,59]],"W"],
-['sat_water',267,16,1,[[0,24],[0,59]],"W"],
-['sun_water',283,16,1,[[0,24],[0,59]],"W"]
+['mon_heat',103,12,1,[[0,24],[0,59],[5,35]],MAX_AGE_MEDIUM,"W"],
+['tues_heat',115,12,1,[[0,24],[0,59],[5,35]],MAX_AGE_MEDIUM,"W"],
+['wed_heat',127,12,1,[[0,24],[0,59],[5,35]],MAX_AGE_MEDIUM,"W"],
+['thurs_heat',139,12,1,[[0,24],[0,59],[5,35]],MAX_AGE_MEDIUM,"W"],
+['fri_heat',151,12,1,[[0,24],[0,59],[5,35]],MAX_AGE_MEDIUM,"W"],
+['sat_heat',163,12,1,[[0,24],[0,59],[5,35]],MAX_AGE_MEDIUM,"W"],
+['sun_heat',175,12,1,[[0,24],[0,59],[5,35]],MAX_AGE_MEDIUM,"W"],
+['mon_water',187,16,1,[[0,24],[0,59]],MAX_AGE_MEDIUM,"W"],
+['tues_water',203,16,1,[[0,24],[0,59]],MAX_AGE_MEDIUM,"W"],
+['wed_water',219,16,1,[[0,24],[0,59]],MAX_AGE_MEDIUM,"W"],
+['thurs_water',235,16,1,[[0,24],[0,59]],MAX_AGE_MEDIUM,"W"],
+['fri_water',251,16,1,[[0,24],[0,59]],MAX_AGE_MEDIUM,"W"],
+['sat_water',267,16,1,[[0,24],[0,59]],MAX_AGE_MEDIUM,"W"],
+['sun_water',283,16,1,[[0,24],[0,59]],MAX_AGE_MEDIUM,"W"]
 ]
 
 CURRENT_TIME_DAY = 0
