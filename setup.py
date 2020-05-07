273d332d0644eda2da8be21f3c9bf274e6846611
Ian Horsley <ianhorsley@cantab.net>
2020-05-07 15:07:43 +0100

    update version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
         return fhandle.read()
 
 setup(name='heatmisercontroller',
-      version='0.43',
+      version='0.44',
       description='Python implementation of Heatmiser protocol for serial connected thermostats ',
       long_description=readme(),
       classifiers=[
