37cc0c489e94b3f8b47d7e917b5aba94922f8d52
Ian Horsley <ianspam@horsley.me.uk>
2022-06-28 20:00:25 +0100

    retryer on first connect

--- heatmisercontroller/adaptor.py
+++ heatmisercontroller/adaptor.py
@@ -78,6 +78,7 @@ class HeatmiserAdaptor():
 
 ### low level serial commands
 
+    @retryer(max_retries=3)
     def connect(self):
         """If not open, open serial port and log settings"""
         if not self.serport.isOpen():
