7f78390ef6bdac774ff2f101b7375c209d1de9e1
Ian Horsley <ianspam@horsley.me.uk>
2022-07-04 21:00:31 +0100

    tidy

--- bin/hm_find_example.py
+++ bin/hm_find_example.py
@@ -2,6 +2,7 @@
 """Gets data from Heatmiser stats repeatedly and displays
 Ian Horsley 2018
 """
+from __future__ import absolute_import
 import os
 import logging
 
@@ -20,14 +21,19 @@ HMN.find_devices(10)
 
 # CYCLE THROUGH ALL CONTROLLERS
 for current_controller in HMN.controllers:
-    print("\r\nGetting all data control %2d in %s *****************************" %(current_controller.address, current_controller.long_name))
+    print("\r\nGetting all data control %2d in %s *****************************" %(
+            current_controller.address, current_controller.long_name))
 
     try:
         current_controller.read_all()
     except HeatmiserResponseError as err:
-        print("C%d in %s Failed to Read due to %s" %(current_controller.address, current_controller.name.ljust(4), str(err)))
+        print("C%d in %s Failed to Read due to %s" %(current_controller.address,
+                current_controller.name.ljust(4), str(err)))
     else:
-        disptext = "C%d Air Temp is %.1f from type %.f and Target set to %d    Boiler Demand %d" % (current_controller.address, current_controller.read_air_temp(), current_controller.read_air_sensor_type(), current_controller.setroomtemp, current_controller.heatingdemand)
+        disptext = "C%d Air Temp is %.1f from type %.f and Target set to %d   Boiler Demand %d"%(
+                    current_controller.address, current_controller.read_air_temp(),
+                    current_controller.read_air_sensor_type(),
+                    current_controller.setroomtemp, current_controller.heatingdemand)
         if current_controller.is_hot_water:
             print("%s Hot Water Demand %d" %(disptext, current_controller.hotwaterdemand))
             current_controller.display_water_schedule()
