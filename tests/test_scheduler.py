b55aef934f09e62be8a628915816603663c165b6
Ian Horsley <ianspam@horsley.me.uk>
2022-07-05 08:19:33 +0100

    minor tidy

--- tests/test_scheduler.py
+++ tests/test_scheduler.py
@@ -1,4 +1,5 @@
 """Unittests for heatmisercontroller.scheduler module"""
+from __future__ import absolute_import
 import unittest
 import logging
 
@@ -11,17 +12,17 @@ class TestSchedulerDayHeat(unittest.TestCase):
     def setUp(self):
         logging.basicConfig(level=logging.ERROR)
         self.func = SchedulerDayHeat()
-            
+
     def test_create(self):
         #unique_address,length,divisor, valid range
         self.func.set_raw_all([7, 0, 21, 9, 0, 12, 17, 0, 21, 22, 30, 15])
         self.func.set_raw('tues_heat', [7, 0, 21, 9, 0, 12, 17, 0, 21, 24, 30, 15])
         self.func.display()
-        
+
     def test_getnextday(self):
         self.assertEqual(3, self.func._get_next_day([2, 0, 0, 0]))
         self.assertEqual(1, self.func._get_next_day([7, 0, 0, 0]))
-        
+
     def test_pad_bad_input(self):
         self.assertRaises(IndexError, self.func.pad_schedule, [1])
         self.assertRaises(IndexError, self.func.pad_schedule, [1, 2])
@@ -40,13 +41,13 @@ class TestSchedulerWeekWater(unittest.TestCase):
     def test_entry_text(self):
         self.assertEqual(self.func.entry_text([12, 12, 14, 30, 15, 00, 15, 30, 17, 00, 24, 00, 24, 00, 24, 00]),"Time 1 On at 12:12 Off at 14:30, Time 2 On at 15:00 Off at 15:30, Time 3 On at 17:00 Time 4 ")
         self.assertEqual(self.func.entry_text([24, 00, 14, 30, 15, 00, 15, 30, 17, 00, 24, 00, 24, 00, 24, 00]),"Time 1 Off at 14:30, Time 2 On at 15:00 Off at 15:30, Time 3 On at 17:00 Time 4 ")
-        
+
     def test_entry(self):
         entry = [12, 12, 14, 30, 15, 00, 15, 30, 17, 00, 24, 00, 24, 00, 24, 00]
         self.func.set_raw('wday_water', entry)
         self.assertEqual(self.func._get_schedule_entry(1), entry)
         self.assertEqual(self.func._get_schedule_entry(5), entry)
         self.assertIsNone(self.func._get_schedule_entry(6))
-            
+
 if __name__ == '__main__':
     unittest.main()
