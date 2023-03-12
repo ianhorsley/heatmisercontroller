9c28f4caea581f9f43f347f12e768fd1286224da
Ian Horsley <ianhorsley@cantab.net>
2023-03-12 19:29:27 +0000

    fix old variable name change

--- bin/hm_find_example.py
+++ bin/hm_find_example.py
@@ -20,14 +20,14 @@ HMN.find_devices(10)
 
 # CYCLE THROUGH ALL CONTROLLERS
 for current_controller in HMN.controllers:
-    print("\r\nGetting all data control %2d in %s *****************************" %(current_controller.address, current_controller.long_name))
+    print("\r\nGetting all data control %2d in %s *****************************" %(current_controller.set_address, current_controller.set_long_name))
 
     try:
         current_controller.read_all()
-    except HeatmiserResponseError as err:
-        print("C%d in %s Failed to Read due to %s" %(current_controller.address, current_controller.name.ljust(4), str(err)))
-    else:
         disptext = "C%d Air Temp is %.1f from type %.f and Target set to %d    Boiler Demand %d" % (current_controller.address, current_controller.read_air_temp(), current_controller.read_air_sensor_type(), current_controller.setroomtemp, current_controller.heatingdemand)
+    except HeatmiserResponseError as err:
+        print("C%d in %s Failed to Read due to %s" %(current_controller.set_address, current_controller.name.ljust(4), str(err)))
+    else:
         if current_controller.is_hot_water:
             print("%s Hot Water Demand %d" %(disptext, current_controller.hotwaterdemand))
             current_controller.display_water_schedule()
